17d03d4ee7f3edb57f949091fc7935df7ed3eb09
phw11 <hwpark11@kakao.com>
2021-09-17 16:47:46 +0900

    feat : Loadbalancer 예제 추가

--- first-service/src/main/java/com/example/firstservice/controller/FirstController.java
+++ first-service/src/main/java/com/example/firstservice/controller/FirstController.java
@@ -2,16 +2,25 @@ package com.example.firstservice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("first-service")
 public class FirstController {
 
   private final Logger log = LoggerFactory.getLogger(getClass());
+
+  private final Environment env;
+
+  public FirstController(Environment env) {
+    this.env = env;
+  }
 
   @GetMapping(path = "welcome")
   public String welcome() {
@@ -27,7 +36,12 @@ public class FirstController {
   }
 
   @GetMapping(path = "check")
-  public String check() {
-    return "Hi, there. This is a message from First Service.";
+  public String check(HttpServletRequest request) {
+
+    log.info("Server port={}", request.getServerPort());
+
+    return String.format(
+        "Hi, there. This is a message from First Service on PORT %s",
+        env.getProperty("local.server.port"));
   }
 }
