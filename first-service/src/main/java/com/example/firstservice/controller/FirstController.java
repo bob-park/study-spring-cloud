4ff0f654a613f1df5a070d1f5af1acfc0f2416cc
phw11 <hwpark11@kakao.com>
2021-09-17 15:35:50 +0900

    feat : route config 예제 추가

--- first-service/src/main/java/com/example/firstservice/controller/FirstController.java
+++ first-service/src/main/java/com/example/firstservice/controller/FirstController.java
@@ -1,15 +1,28 @@
 package com.example.firstservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("welcome")
+@RequestMapping("first-service")
 public class FirstController {
 
-  @GetMapping
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
+  @GetMapping(path="welcome")
   public String welcome() {
     return String.format("Welcome to the First Service");
+  }
+
+  @GetMapping(path = "message")
+  public String message(@RequestHeader("first-request") String header) {
+
+    log.info("first-request : {}", header);
+
+    return String.format("message : %s", header);
   }
 }
--- second-service/src/main/java/com/example/secondservice/controller/SecondController.java
+++ second-service/src/main/java/com/example/secondservice/controller/SecondController.java
@@ -1,15 +1,28 @@
 package com.example.secondservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("welcome")
+@RequestMapping("second-service")
 public class SecondController {
 
-  @GetMapping
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
+  @GetMapping(path="welcome")
   public String welcome() {
     return String.format("Welcome to the Second Service");
+  }
+
+  @GetMapping(path = "message")
+  public String message(@RequestHeader("second-request") String header) {
+
+    log.info("second-request : {}", header);
+
+    return String.format("message : %s", header);
   }
 }
