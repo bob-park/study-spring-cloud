a8cf485f2a63cff9b8bcae341285d3460dd9f166
phw11 <hwpark11@kakao.com>
2021-09-17 15:01:37 +0900

    feat : api gateway test service module 추가

---
+++ fisrt-service/src/main/java/com/example/fisrtservice/FisrtServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.fisrtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisrtServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FisrtServiceApplication.class, args);
+  }
+}
---
+++ fisrt-service/src/test/java/com/example/fisrtservice/FisrtServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.fisrtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisrtServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
