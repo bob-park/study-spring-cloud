90f97c8696e5ecb5613420c70ecdfeddcdc753b0
phw11 <hwpark11@kakao.com>
2021-09-18 16:30:45 +0900

    feat : 설명 주석 추가

--- catalog-service/src/main/resources/data.sql
+++ catalog-service/src/main/resources/data.sql
@@ -1,3 +1,5 @@
+-- spring boot 2.5 버전 이상부터 flyway 등 db initialize tool 과 통합하기 위해, 사용하지 않는 걸 추천
+-- 동작도 잘 안함
 insert into catalog (product_id, product_name, stock, unit_price)
 values ('CATALOG-001', 'Berlin', 100, 1500);
 insert into catalog (product_id, product_name, stock, unit_price)
