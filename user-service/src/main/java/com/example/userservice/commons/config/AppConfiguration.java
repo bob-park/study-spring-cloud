3b8e2e71f0572235d1acf3154cbc68394054cfdd
phw11 <hwpark11@kakao.com>
2021-09-21 16:02:50 +0900

    feat : api gateway 에 spring cloud config 적용

--- user-service/src/main/java/com/example/userservice/commons/config/AppConfiguration.java
+++ user-service/src/main/java/com/example/userservice/commons/config/AppConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.userservice.commons.config;
 
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,5 +12,15 @@ public class AppConfiguration {
   @Bean
   public BCryptPasswordEncoder getPasswordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  /**
+   * actuator 의 httptrace 를 사용하기 위해 bean 에 등록해주어야 한다.
+   *
+   * @return
+   */
+  @Bean
+  public HttpTraceRepository httpTraceRepository() {
+    return new InMemoryHttpTraceRepository();
   }
 }
