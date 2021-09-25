6f267234c0b0ac135ba2419b32bf594e93b15166
phw11 <hwpark11@kakao.com>
2021-09-25 16:16:23 +0900

    refactor : 예외부분 제거

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -87,13 +87,13 @@ public class OrderController {
 
     List<Order> orders = orderService.getOrdersByUserId(userId);
 
-    try {
-      Thread.sleep(1000);
-
-      throw new Exception("Issue!!!");
-    } catch (InterruptedException e) {
-      log.warn(e.getMessage());
-    }
+//    try {
+//      Thread.sleep(1000);
+//
+//      throw new Exception("Issue!!!");
+//    } catch (InterruptedException e) {
+//      log.warn(e.getMessage());
+//    }
 
     log.info("After retrieve orders data");
 
